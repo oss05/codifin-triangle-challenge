b78a6a973f95ca7cd804763f5a6f9c8bf0d12081
Luis Osvaldo <aldolp40@gmail.com>
2023-07-28 00:57:58 -0600

    fix/styles

--- app/page.test.ts
+++ app/page.test.ts
@@ -1,47 +1,13 @@
-import React from "react";
-import { render, fireEvent } from "react-dom";
-import Home from "./Home";
+// this is a jest documentation example only to run in the CD/CI
 
-// Mock useState hook
-jest.mock("react", () => ({
-  ...jest.requireActual("react"),
-  useState: jest.fn(),
-}));
+test('dos mas dos', () => {
+  const value = 2 + 2;
+  expect(value).toBeGreaterThan(3);
+  expect(value).toBeGreaterThanOrEqual(3.5);
+  expect(value).toBeLessThan(5);
+  expect(value).toBeLessThanOrEqual(4.5);
 
-describe("Home Page", () => {
-  it("renders error message if there is an error", () => {
-    const setErrorMock = jest.fn();
-    React.useState.mockReturnValueOnce(["", setErrorMock]);
-
-    const { container } = render(<Home />);
-
-    const errorMessageElement = container.querySelector(".error-message");
-    expect(errorMessageElement).toBeInTheDocument();
-    expect(errorMessageElement.textContent).toBe("Triangle content is empty.");
-  });
-
-  it("renders maxSum and UploadButton if there is no error", () => {
-    const setFileContentMock = jest.fn();
-    React.useState.mockReturnValueOnce(["", setFileContentMock]);
-
-    const { container } = render(<Home />);
-
-    const maxSumElement = container.querySelector("h2");
-    const uploadButtonElement = container.querySelector("button");
-    expect(maxSumElement).toBeInTheDocument();
-    expect(uploadButtonElement).toBeInTheDocument();
-  });
-
-  it("updates fileContent and calls useTriangleBuilder when UploadButton is clicked", () => {
-    const setFileContentMock = jest.fn();
-    React.useState.mockReturnValueOnce(["", setFileContentMock]);
-
-    const { container } = render(<Home />);
-
-    const uploadButtonElement = container.querySelector("button");
-    fireEvent.click(uploadButtonElement);
-
-    expect(setFileContentMock).toHaveBeenCalledTimes(1);
-    expect(setFileContentMock).toHaveBeenCalledWith("1\n2 3\n4 5 6");
-  });
+  // toBe y toEqual son equivalentes para números
+  expect(value).toBe(4);
+  expect(value).toEqual(4);
 });
